7f12999256d70f7f3d31978c8ae434607d968c4f
Kenmogne Thimotee <44063958+KenmogneThimotee@users.noreply.github.com>
2023-02-07 18:55:30 +0100

    Update urls.py

--- VReservation/urls.py
+++ VReservation/urls.py
@@ -21,9 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projet.urls')),
-    path('djrichtextfield/', include('djrichtextfield.urls'))
-
+    path('', include('projet.urls'))
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns  += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
